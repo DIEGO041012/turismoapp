dd0daecdf6003af46a8cac2ed2571bef209bba82
mariana <marianaortegaortiz92@gmail.com>
2023-08-16 12:25:25 -0500

    nuevo proyecto back end

---
+++ src/main/java/com/example/turismoapp/modelos/Empresa.java
@@ -0,0 +1,9 @@
+package com.example.turismoapp.modelos;
+
+public class Empresa {
+    private Integer id;
+    private String nit;
+    private String nombre;
+    private Integer ubicacion;
+    private 
+}
---
+++ src/main/java/com/example/turismoapp/modelos/oferta.java
@@ -0,0 +1,13 @@
+package com.example.turismoapp.modelos;
+
+import java.time.LocalDate;
+
+public class oferta {
+    private Integer id;
+    private String titulo;
+    private String descripcion;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private Double costoPersona;
+}
+
